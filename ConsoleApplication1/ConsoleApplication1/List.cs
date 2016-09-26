e47238a5eb8b83b36734cfbec8dcca1becc17992
ConChimNon <linhnaisermtv02@gmail.com>
2016-09-26 09:28:11 +0700

    FS

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List<T>
+    {
+        private Element<T> head;
+
+        internal Element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+    }
+}
